23f48c9873e521a69a3dd48689ce78837fbf302a
S G <Siraj@Ss-MacBook-Pro.local>
2020-08-26 23:34:04 -0400

    added URL and title

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+DATA_URL = (
+    "/Desktop/Vehicle-Collisions-NYC"
+)
+
+st.title("Motor Vehicle Collisions in NYC")
+st.markdown("This dashboard analyzes motor vehicle collisions in NYC")
